b2896a6360b7c427ac2d3e0f675e71c7cbcff738
Zdenka Gerslova <gerslovaz@math.muni.cz>
2024-07-19 20:49:58 +0200

    First easy interactive waveform function.

--- R/interactive_waveforms.R
+++ R/interactive_waveforms.R
@@ -1,27 +1,24 @@
-interactive_waveforms <- function() {
+interactive_waveforms <- function(data, subject, sensor) {
+  data <- data %>%
+    dplyr::filter(subject == {{ subject }} & (electrode == {{ sensor }}))  %>%
+    dplyr::select(time, signal, epoch)
+  data <- data %>%
+    group_by(time) %>%
+    mutate(average = mean(signal, na.rm = TRUE))
+
+  label <- rlang::englue("Subject {{ subject }}, electrode { sensor }")
+
+  curv_epoch <- data %>%
+    group_by(epoch) %>%
+    plot_ly(x = ~time * 4 - 1004, y = ~signal, color = ~epoch, colors = rainbow(50),
+            type = "scatter",  mode = "lines")
+  curv_epoch %>%
+    add_trace(x = ~time * 4 - 1004, y = ~average, type = 'scatter', mode = 'lines',
+              line = list(color = 'black'),
+              name = 'Average') %>%
+    layout(xaxis = list(title = "Time (ms)"),
+           yaxis = list(title = TeX("\\mu V")),
+           showlegend = F, title = label) %>%
+    config(mathjax = 'cdn')
 
 }
-
-
-
-controls01 <- controls %>%
-  dplyr::filter(subject == 1 & (electrode == "E65"))  %>%
-  dplyr::select(time, signal, epoch)
-
-controls01df <- as.data.frame(controls01)
-controls01df <- controls01df %>%
-  group_by(time) %>%
-  mutate(average = mean(signal, na.rm = TRUE))
-controls01df$epoch <- factor(controls01df$epoch)
-curv_epoch <- controls01df %>%
-  group_by(epoch) %>%
-  plot_ly(x = ~time * 4 - 1004, y = ~signal, color = ~epoch,
-          type = "scatter",  mode = "lines")
-curv_epoch %>%
-  add_trace(x = ~time * 4 - 1004, y = ~average, type = 'scatter', mode = 'lines',
-            line = list(color='red'),
-            name = 'Average') %>%
-  layout(xaxis = list(title = "Time (ms)"),
-         yaxis = list(title = TeX("\\mu V")),
-         showlegend = F, title='Subject HC01, electrode E65') %>%
-  config(mathjax = 'cdn')
