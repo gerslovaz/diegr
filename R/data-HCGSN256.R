fa70ee4ba269d136ad27159f294bbcf12b42e166
Zdenka Gerslova <gerslovaz@math.muni.cz>
2024-09-25 14:32:17 +0200

    edit HCGSN256 description

--- R/data-HCGSN256.R
+++ R/data-HCGSN256.R
@@ -1,7 +1,7 @@
 #' @title Coordinates of 256-channel HCGSN sensors
 #'
-#' @description A file containing coordinates ....
-#' The coordinates belong to 256-channel HydroCel Geodesic Sensor Net template montage (averaging ... montages)
+#' @description A file containing the Cartesian coordinates of high-density EEG sensor positions in 3D space on the scalp surface and their projection into 2D space.
+#' The coordinates belong to 256-channel HydroCel Geodesic Sensor Net average template montage.
 #'
 #' @docType data
 #'
@@ -11,15 +11,21 @@
 #' \describe{
 #'   \item{number}{Number of sensor (according to EGI GSN Technical Manual)}
 #'   \item{D2}{A tibble with 2 columns containing x and y coordinates of sensors in 2D}
-#'   \item{D3}{A tibble with 3 columns containing x, y and z coordinates of sensors in 3D}
+#'   \item{D3}{A tibble with 3 columns containing x, y and z coordinates of sensors in 3D. See 'Details' for more information.}
 #'   \item{ROI}{Factor containing the name of the region to which the corresponding sensor belongs.
 #'   The levels are: "central", "frontal", "occipital", "parietal", "temporal".
 #'   The regions were determined by an expert from Central European Institute of Technology, Masaryk University, Brno, Czech Republic.}
 #' }
 #'
+#' @details
+#' The axis orientation in the 3D case is as follows: the x axis runs left (negative) to right (positive); the y axis runs posterior (negative) to anterior (positive); and the z axis runs inferior (negative) to superior (positive).
+#' The reference electrode (Cz) is fixed at point (0, 0, Z), where Z is the positive height of Cz. The nasion is fixed at (0, Y, Z). Since both the nasion and Cz are always fixed at x = 0, they are assumed to be in the same y plane. The origin is the center of the head, defined as the center of a sphere fit to fiducial points above the plane made up of the Left Preauricular Point (LPA), the Right Preauricular Point (RPA), and the nasion.
+#'
+#' The 2D coordinates were obtained as a stereographic projection of the sensor positions in 3D space.
+#'
 #' @keywords dataset
 #'
-#' @references EGI Geodesic Sensor Net Technical Manual (2024)
+#' @references EGI Geodesic Sensor Net Technical Manual (2024), www.egi.com
 #'
 #' @source Central European Institute of Technology, Masaryk University, Brno, Czech Republic.
 #'
