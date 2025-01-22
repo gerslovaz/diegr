fb949780d4c86b62b660588d63d375a260253f7f
Zdenka Gerslova <gerslovaz@math.muni.cz>
2025-01-22 22:25:25 +0100

    Edit boxplot_epoch() for DB input and remove some non-effective input checks for faster computing.

--- R/boxplot_epoch.R
+++ R/boxplot_epoch.R
@@ -1,13 +1,12 @@
 #' Plot interactive boxplots of epochs
 #'
 #' @description
-#' Function for plotting interactive boxplots of EEG amplitude in individual epochs for selected subject and channel within the chosen time interval. The interactive plotly output enables to easily determine the epoch number from which outliers come.
+#' Function for plotting interactive boxplots of EEG amplitude in individual epochs for selected subject and channel within the chosen time interval. The interactive plotly output enables to easily determine the epoch number from which outliers come and also allows to easily edit the image layout.
 #'
 #' @param data A data frame or a database table with EEG dataset. Required columns: subject, sensor, epoch, time, signal.
 #' @param subject An integer or character ID of selected subject to plot.
 #' @param channel An integer or character ID of channel to plot.
 #' @param time_lim A character vector with time range to plot. If not defined, the first ten time points are plotted.
-#' @param ... Additional arguments affecting the plot.
 #'
 #' @return A plotly object.
 #'
@@ -24,8 +23,7 @@
 boxplot_epoch <- function(data,
                          subject,
                          channel,
-                         time_lim = NULL,
-                         ...) {
+                         time_lim = NULL) {
 
   if (missing(subject)) {
     stop("Argument 'subject' is missing, with no default.")
@@ -39,34 +37,38 @@ boxplot_epoch <- function(data,
   missing_cols <- setdiff(required_cols, colnames(data))
 
   if (length(missing_cols) > 0) {
-    stop(paste("The following required columns are missing:",
+    stop(paste("The following required data columns are missing:",
                paste(missing_cols, collapse = ", ")))
   }
 
   if (is.null(time_lim)) {
-    min.t <- min(data$time)
+    min.t <- min(dplyr::pull(data, time))
     time_lim <- c(min.t:(min.t + 9))
     warning("The argument 'time_lim' was not defined, the first ten time points from data are plotted.")
   }
 
   # check time range
-  min_lim <- min(time_lim)
-  max_lim <- max(time_lim)
-
-  if (!any(data$time >= min_lim & data$time <= max_lim)) {
-    stop("There are no time values in the input data within the range defined by 'time_lim'.")
-  }
+  #  min_lim <- min(time_lim)
+  #  max_lim <- max(time_lim)
+  #
+  # if (!any(dplyr::pull(data, time) >= min_lim & dplyr::pull(data, time) <= max_lim)) {
+  #   stop("There are no time values in the input data within the range defined by 'time_lim'.")
+  # }
 
   db_sub <- data |>
     dplyr::filter(subject == {{ subject }} & (sensor == {{ channel }}) & time %in% time_lim)  |>
     dplyr::select(time, signal, epoch)
-  db_df <- as.data.frame(db_sub)
-  db_df$epoch <- factor(db_df$epoch)
+  db_df <- collect(db_sub) #as.data.frame(db_sub)
+  #db_df$epoch <- factor(db_df$epoch)
 
   fig <- plot_ly(db_df, x = ~time, y = ~signal) |>
     add_boxplot(hovertext = paste("Epoch :", db_df$epoch))
   fig2 <- fig |> layout(xaxis = list(title = "Time point"),
-                         yaxis = list(title = TeX("\\mu V"))) |>
+                         yaxis = list(title = TeX("\\mu V")),
+                        title = paste("Subject ", {{ subject }}, ", channel ", {{ channel }}, sep = "")) |>
     config(mathjax = 'cdn')
   fig2
 }
+
+
+
