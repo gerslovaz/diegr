ff46227bca0d8b210ff94f41eac4d5c56ad4c2e2
Zdenka Gerslova <gerslovaz@math.muni.cz>
2025-02-13 23:00:53 +0100

    First version of baseline_correction()

---
+++ R/baseline_correction.R
@@ -0,0 +1,59 @@
+#' Baseline correction
+#'
+#' @description
+#' Function for computing amplitude corrected to the selected baseline.
+#'
+#'
+#' @param data A data frame, tibble or a database table with input data, required columns: subject, sensor, time, signal, epoch.
+#' @param base.int Numeric character vector including time points to use as a baseline. It must include all required values, so for baseline between time points 125 and 250 write \code{base.int = 125:250}.
+#' @param type A character indicating the type of baseline correction. Only the value \code{'absolute'} is available at this moment.
+#'
+#' @return A data frame/tibble with added two columns:
+#' \item{signal_base}{signal corrected on chosen baseline}
+#' \item{baseline}{a value of baseline for the given epoch}
+#' @export
+#'
+#' @examples
+#' # Computing baseline correction on first 10 points, sensor "E1"
+#' data("epochdata")
+#' data01 <- epochdata |> dplyr::filter(subject == 1 & sensor == "E1")
+#' basedata <- baseline_correction(data01, base.int = 1:10, type = 'absolute')
+#' # Plotting raw (black line) and corrected (red line) signal for epoch 1
+#' plot(basedata$signal[1:50], type = "l", ylim = c(-40, 20), xlab = "time point", ylab = "amplitude")
+#' lines(basedata$signal_base[1:50], col = "red")
+baseline_correction <- function(data, base.int, type = 'absolute') {
+  required_cols <- c("time", "signal", "epoch", "subject", "sensor")
+  missing_cols <- setdiff(required_cols, colnames(data))
+
+  if (length(missing_cols) > 0) {
+    stop(paste("The following required columns in 'data' are missing:",
+               paste(missing_cols, collapse = ", ")))
+  }
+
+  newdata <- data |>
+    dplyr::select(subject, sensor, epoch, time, signal)
+  newdata <- collect(newdata)
+
+  newdata <- newdata |>
+    dplyr::group_by(subject, sensor, epoch) |>
+    dplyr::mutate(baseline = mean(signal[{ base.int }])) |>
+    dplyr::mutate(signal_base = signal - baseline) |>
+    dplyr::ungroup()
+
+  return(newdata)
+
+}
+
+# E1 <- epochdata |>
+#   dplyr::filter(sensor == "E1", subject == 1)
+#
+#
+# d03 <- E12bothbase |>
+#   filter(subject == 2 & sensor == "E1")
+#
+# epocha1 <- epochdata |>
+#   filter(subject == 2 & sensor == "E1" & epoch == 1)
+#
+# E1bothbase |>
+#   filter(subject == 2 & epoch == 1) |>
+#   select(signal_base)
